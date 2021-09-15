b0f39dc1e5daa796dc2ecaa88d2a21f0464b412e
71M-M414 <guilherme.vtm2@gmail.com>
2021-09-15 12:13:43 -0300

    estrutura basica

--- typing/src/App.js
+++ typing/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">Gui</span>
+        <span className="remainder">lherme</span>
+      </div>
+      <div className="typed-keys">abcgsGui</div>
+      <div className="completed-words">
+        <ol>
+          <li>Pokemon</li>
+          <li>Zoroark</li>
+          <li>Infernape</li>
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
